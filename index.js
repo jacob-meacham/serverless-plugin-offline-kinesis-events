651fa806d40cee80e08ba52f40118a5a12481643
Matthew Sills <matt.sills@dopplerlabs.com>
2017-01-16 09:56:22 -0800

    Re CR Add a bootstrap script eslint and editorconfig Lint clean rm -rf => rimraf README.md, extra dev run script to put events

--- index.js
+++ index.js
@@ -1,122 +1,153 @@
-'use strict';
-
-const _ = require('lodash');
-const AWS = require('aws-sdk');
-const BB = require('bluebird');
+import * as _ from 'lodash'
+import * as AWS from 'aws-sdk'
+import BB from 'bluebird'
+import winston from 'winston'
 
 /**
  * Based on ServerlessWebpack.run
  * @param stats
  */
 function getRunnableLambda(slsWebpack, stats, functionName) {
-    return (event) => {
-        const handler = slsWebpack.loadHandler(stats, functionName, true);
-        const context = slsWebpack.getContext(functionName);
-        return new BB(
-            (resolve, reject) => handler(
-                event,
-                context,
-                (err, res) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(res);
-                    }
-                }
-            ));
-    }
+  return (event) => {
+    const handler = slsWebpack.loadHandler(stats, functionName, true)
+    const context = slsWebpack.getContext(functionName)
+    return new BB(
+      (resolve, reject) => handler(
+        event,
+        context,
+        (err, res) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(res)
+          }
+        }
+      ))
+  }
 }
+
+const MAX_CONSECUTIVE_ERRORS = 10
 
 class ServerlessOfflineKinesisEvents {
-    constructor(serverless, options) {
-        this.serverless = serverless;
-        this.options = options;
-        // Only meaningful for AWS
-        this.provider = 'aws';
+  constructor(serverless, options) {
+    this.serverless = serverless
+    this.options = options
+    // Only meaningful for AWS
+    this.provider = 'aws'
 
-        // No commands to run
-        // TODO(msills): Allow this to be run independently
-        this.commands = {};
-        // Run automatically as part of the deploy
-        this.hooks = {
-            'after:offline:start': () => BB.bind(this).then(this.runWatcher)
-        };
-
-        // Registry of streams to lists of functions
-        this.registry = null;
-
+    // No commands to run
+    // TODO(msills): Allow this to be run independently
+    this.commands = {}
+    // Run automatically as part of the deploy
+    this.hooks = {
+      'after:offline:start': () => BB.bind(this).then(this.runWatcher)
     }
+  }
 
-    static async createRegistry(serverless) {
-        // Get a handle on the compiled functions
-        // TODO(msills): Do not rely on this plugin.
-        const slsWebpack = _.find(serverless.pluginManager.plugins, (p) => p.constructor.name === 'ServerlessWebpack');
-        const compileStats = await slsWebpack.compile();
+  static async createRegistry(serverless) {
+    // Get a handle on the compiled functions
+    // TODO(msills): Do not rely on this plugin.
+    const slsWebpack = _.find(serverless.pluginManager.plugins, p => p.constructor.name === 'ServerlessWebpack')
+    const compileStats = await slsWebpack.compile()
 
-        let registry = {};
-        for (let functionName of _.keys(serverless.service.functions)) {
-            const func = serverless.service.functions[functionName];
-            // Get the list of streams for the function
-            const streamEvents = _.filter(func.events || [], (e) => 'stream' in e);
-            for (let s of streamEvents) {
-                const streamName = s.stream.arn.split('/').slice(-1)[0];
-                registry[streamName] = registry[streamName] || [];
-                registry[streamName].push(getRunnableLambda(slsWebpack, compileStats, functionName));
-            }
-        }
-        return registry;
+    const registry = {}
+    for (const functionName of _.keys(serverless.service.functions)) {
+      const func = serverless.service.functions[functionName]
+      // Get the list of streams for the function
+      const streamEvents = _.filter(func.events || [], e => 'stream' in e)
+      for (const s of streamEvents) {
+        const streamName = s.stream.arn.split('/').slice(-1)[0]
+        registry[streamName] = registry[streamName] || []
+        registry[streamName].push(getRunnableLambda(slsWebpack, compileStats, functionName))
+      }
     }
+    return registry
+  }
 
-    async runWatcher() {
-        // Lazily load the registry
-        if (this.registry === null) {
-            this.registry = await ServerlessOfflineKinesisEvents.createRegistry(this.serverless);
+  static async _runLambdas(kinesis, registry, streamIterators) {
+    winston.debug(`Polling Kinesis streams: ${JSON.stringify(_.keys(registry))}`)
+    // Get the new values for each stream
+    // name -> [fetch result]
+    const recordsResults = await BB.props(
+      _.mapValues(
+        streamIterators,
+        iter => kinesis.getRecords({
+          ShardIterator: iter,
+          Limit: 100
+        }).promise()))
+
+    // Wait for the functions to execute
+    await BB.all(_.chain(recordsResults)
+      .entries()
+      .flatMap(([name, result]) => {
+        if (streamIterators[name] === null) {
+          winston.warn(`Iterator for stream '${name}' + is closed`)
         }
+        winston.debug(`Stream '${name}' returned ${result.Records.length} records`)
+        // Parse the records
+        const records = _.map(result.Records, r => JSON.parse(r.Data.toString()))
+        // Apply the functions that use that stream
+        return registry[name].map(f => f({ Records: records }))
+      })
+      .value())
+    // Update the stream iterators and return
+    return _.mapValues(recordsResults, result => result.NextShardIterator)
+  }
 
-        // Create the Kinesis client
-        const config = this.serverless.service.custom.offlineKinesisEvents;
-        const kinesis = new AWS.Kinesis({
-            endpoint: config.host + ':' + config.port,
-            region: config.region,
-            apiVersion: '2013-12-02',
-            sslEnabled: config.sslEnabled || false
-        });
+  async runWatcher() {
+    // Create the Kinesis client
+    const config = this.serverless.service.custom.offlineKinesisEvents
+    const kinesis = new AWS.Kinesis({
+      endpoint: `${config.host}:${config.port}`,
+      region: config.region,
+      apiVersion: '2013-12-02',
+      sslEnabled: config.sslEnabled || false
+    })
 
-        // Right now, the stream iterators are local to this run. Eventually, we'll persist this somewhere
-        let streamIterators = {};
-        // Get the first shard for every element in the registry
-        for (let s of _.keys(this.registry)) {
-            // Describe the stream
-            const stream = await kinesis.describeStream({
-                StreamName: s
-            }).promise();
-            const shardId = stream.StreamDescription.Shards[0].ShardId;
-            // Get an iterator for the shard
-            const result = await kinesis.getShardIterator({ShardId: shardId, ShardIteratorType: 'TRIM_HORIZON', StreamName: s}).promise();
-            streamIterators[s] = result.ShardIterator;
+    // Load the registry
+    const registry = await ServerlessOfflineKinesisEvents.createRegistry(this.serverless)
+    // Get the first shard for every element in the registry
+    // Right now, the stream iterators are local to this run. Eventually, we'll persist this somewhere
+    let streamIterators = await BB.props(
+      _.chain(registry)
+      // Grab keys
+        .keys()
+        // Map to [name, stream description promise]
+        .map(name => [name, kinesis.describeStream({ StreamName: name }).promise()])
+        // Map to [name, iterator promise]
+        .map(([name, descP]) => {
+          const iterP = descP.then(desc => kinesis.getShardIterator({
+            ShardId: desc.StreamDescription.Shards[0].ShardId,
+            ShardIteratorType: 'TRIM_HORIZON',
+            StreamName: name
+          }).promise())
+          return [name, iterP]
+        })
+        // Back to an object
+        .fromPairs()
+        // Extract iterators
+        .mapValues(iterP => iterP.then(iter => iter.ShardIterator))
+        // Grab the value
+        .value())
+
+    let consecutiveErrors = 0
+    while (true) { // eslint-disable-line no-constant-condition
+      winston.debug(`Polling Kinesis streams: ${JSON.stringify(_.keys(registry))}`)
+      try {
+        streamIterators = await ServerlessOfflineKinesisEvents._runLambdas(kinesis, registry, streamIterators) // eslint-disable-line
+      } catch (err) {
+        consecutiveErrors += 1
+        if (consecutiveErrors > MAX_CONSECUTIVE_ERRORS) {
+          winston.error(`Exceeded maximum number of consecutive errors (${MAX_CONSECUTIVE_ERRORS})`)
+          throw err
         }
+        winston.error(`Failed to run Lambdas with error ${err.stack}. Continuing`)
+      }
 
-        while (true) {
-            this.serverless.cli.log('Polling Kinesis streams: ' + JSON.stringify(_.keys(this.registry)));
-            for (let s of _.keys(this.registry)) {
-                if (streamIterators[s] === null) {
-                    this.serverless.cli.warn("Iterator for stream '" + s + "' is closed");
-                }
-                // Get all events from the stream
-                const result = await kinesis.getRecords({ShardIterator: streamIterators[s], Limit: 100}).promise();
-                this.serverless.cli.log('Stream ' + s + ' returned ' + result.Records.length + ' records');
-                const records = _.map(result.Records, (r) => JSON.parse(r.Data.toString()));
-                // Process the functions
-                for (let f of this.registry[s]) {
-                    f({Records: records});
-                }
-                // Record the next iterator
-                streamIterators[s] = result.NextShardIterator;
-            }
-            await BB.delay(config.intervalMillis);
-
-        }
+      // Wait a bit
+      await BB.delay(config.intervalMillis) // eslint-disable-line no-await-in-loop
     }
+  }
 }
 
-module.exports = ServerlessOfflineKinesisEvents;
+module.exports = ServerlessOfflineKinesisEvents
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,50 @@
 /**
  * Created by msills on 1/11/17.
  */
-var webpack = require('webpack');
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
 
-module.exports = {
-    entry: ['./index.js'],
-    output: {
-        path: __dirname + "/build",
-        filename: 'bundle.js',
-        libraryTarget: 'umd'
-    },
-    node: {
-        __filename: true,
-        __dirname: true
-    },
-    target: 'node',
-    externals: [
-        nodeExternals()
-    ],
-    resolve: {
-        extensions: ['', '.js']
-    },
-    devtool: 'source-map',
-    plugins: [
-        // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        // new webpack.optimize.DedupePlugin(),
-        // new webpack.optimize.OccurenceOrderPlugin(),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         unused: true,
-        //         dead_code: true,
-        //         warnings: false,
-        //         drop_debugger: true
-        //     }
-        // })
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015'],
-                    plugins: [
-                        "transform-es2015-template-literals",
-                        "transform-object-rest-spread",
-                        "transform-async-to-generator"
-                    ]
-                }
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    }
-};
+const env = process.env.NODE_ENV || 'development'
+const DEV = env === 'development'
+
+const config = {
+  entry: ['./index.js'],
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
+  },
+  node: {
+    __filename: true,
+    __dirname: true
+  },
+  target: 'node',
+  externals: [
+    nodeExternals()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devtool: 'source-map',
+  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  }
+}
+
+if (DEV) {
+  config.devtool = 'source-map'
+}
+
+module.exports = config
